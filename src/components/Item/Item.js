6ccd1b859f20394c737e994928f431f097372bac
Daria Chernovol <fabulus.kg.dk@gmail.com>
2019-11-23 11:20:17 +0600

    Created react app

---
+++ src/components/Item/Item.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Item.css';
+
+const Item = props => {
+    return (
+        <div className='item'>
+            <img src={props.image} alt="#"/>
+            <p>{props.name}</p>
+            <p>{props.price} KGS</p>
+        </div>
+    );
+};
+
+export default Item;
